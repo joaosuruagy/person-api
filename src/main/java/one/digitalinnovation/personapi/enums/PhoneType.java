d0488ff26c87d62da8e14c0028b47318c54a40d6
joaosuruagy <joaosuruagy@gmail.com>
2021-07-26 14:58:19 -0300

    Developed person create operation

---
+++ src/main/java/one/digitalinnovation/personapi/enums/PhoneType.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.personapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+    NOME ("Home"),
+    MOBILE("Mobile"),
+    COMMERCIAL("Commercial");
+
+    private final String description;
+}
